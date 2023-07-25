a52b2ef54ecbe884efe68475d090b4d9e41b2415
Abhi Success <abhisuccess499@gmail.com>
2023-07-25 21:18:59 +0530

    -Aadded files

---
+++ backup.py
@@ -0,0 +1,11 @@
+#12 -> # import json
+# if "start running" in sptext().lower():
+#57 -> # elif "right now" in data1:
+            #     time = datetime.datetime.now().strftime("%I%M%p")
+            #     txspeech(time)
+#87-> # elif "joke" in data1:
+            #     jokess = pyjokes.get_jokes(language="en", category="neutral")
+            #     print(jokess)
+            #     txspeech(jokess)
+#221 ->  # else:
+#     print("Sorry, i couldn't get your voice")
